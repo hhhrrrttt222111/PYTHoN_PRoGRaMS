86b581286d49b7e50243c28e98f088299766479f
hhhrrrttt222111 <43471295+hhhrrrttt222111@users.noreply.github.com>
2020-02-22 17:58:18 +0530

    Update password_generator.py

--- password/password_generator.py
+++ password/password_generator.py
@@ -5,17 +5,49 @@ from random import randrange
 
 
 char = f"[/*-+!@#$%^&*()+:;?,.<>]"
-num1 = f"[1-9]"
+num1 = "0123456789"
 small = s.ascii_lowercase
 caps = s.ascii_uppercase
-passwd = char + num1 + small + caps
+Apasswd = char + num1 + small + caps
+passwd = num1 + small
+
+
+
+
+
+class Gen_Pass_Adv:
+
+    def __init__(self, number, _vchar):
+        self.char_length = len(_vchar)
+        self.default = 13
+        self.num = number
+        self.password = ""
+        self.password_len = 0
+        self.vchar = _vchar
+        self.gen = self.generate()
+
+    def generate(self):
+        length = 0
+        try:
+            length = int(self.num)
+        except ValueError:
+            length = self.default
+
+
+        while self.password_len is not length:
+            pick = randrange(1,self.char_length)
+            self.password_len += 1
+            self.password += Apasswd[pick]
+
+        print(self.password)
+
 
 
 class Gen_Pass:
 
     def __init__(self, number, _vchar):
         self.char_length = len(_vchar)
-        self.default = 13
+        self.default = 10
         self.num = number
         self.password = ""
         self.password_len = 0
@@ -38,8 +70,23 @@ class Gen_Pass:
         print(self.password)
 
 
+
+
 if "__main__" == __name__:
+
+    print("Choose Password Strength")
+    print("Strong : Enter 1")
+    print("Weak : Enter 2")
+    ch = int(input())
+
     print("Enter Length of Password Required")
     num = input("")
-    Gen_Pass(num , passwd)
+
+    if ch == 1:
+        Gen_Pass_Adv(num, Apasswd)
+    else:
+        Gen_Pass(num, passwd)
+
+
+
 
